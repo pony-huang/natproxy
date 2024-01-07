bf5281632dad4249c377c001fdaef5d63fd91a36
PonKing <gafulungatie@gmail.com>
2024-01-07 21:03:43 +0800

    refactor: refactor ServerApplication.java

--- server/src/main/java/org/github/ponking66/ServerApplication.java
+++ server/src/main/java/org/github/ponking66/ServerApplication.java
@@ -1,7 +1,9 @@
 package org.github.ponking66;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.*;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -22,7 +24,6 @@ import org.github.ponking66.handler.*;
 import org.github.ponking66.protoctl.NettyMessageDecoder;
 import org.github.ponking66.protoctl.NettyMessageEncoder;
 import org.github.ponking66.util.ObjectUtils;
-
 
 import java.io.File;
 import java.io.IOException;
@@ -58,23 +59,42 @@ public class ServerApplication implements Application {
 
     @Override
     public void start() throws Exception {
-        initBootstrapServer();
+        initServerBootstrap(false, null, ProxyConfig.server().getPort());
+        LOGGER.info("server start success");
         TLSConfig tls = ProxyConfig.server().getTls();
         if (tls != null && tls.isEnable() &&
                 ObjectUtils.isNotEmpty(tls.getCaFile()) &&
                 ObjectUtils.isNotEmpty(tls.getKeyFile()) &&
                 ObjectUtils.isNotEmpty(tls.getKeyCertChainFile())) {
-            initSSLBootstrapServer(tls);
+            initServerBootstrap(true, tls, ProxyConfig.server().getTls().getPort());
+            LOGGER.info("ssl server start success");
         }
     }
 
-    private void initBootstrapServer() throws InterruptedException, ExecutionException {
+    /**
+     * 初始化服务器引导程序
+     *
+     * @param isSsl 是否使用SSL
+     * @param tls TLS配置
+     * @param port 端口号
+     * @throws InterruptedException 线程中断异常
+     * @throws ExecutionException  执行异常
+     * @throws IOException          IO异常
+     */
+    private void initServerBootstrap(boolean isSsl, TLSConfig tls, int port) throws InterruptedException, ExecutionException, IOException {
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel ch) throws Exception {
                         ChannelPipeline pipeline = ch.pipeline();
+                        if (isSsl) {
+                            File crt = new File(tls.getKeyCertChainFile());
+                            File key = new File(tls.getKeyFile());
+                            File ca = new File(tls.getCaFile());
+                            SslContext sslContext = SslContextBuilder.forServer(crt, key).trustManager(ca).clientAuth(ClientAuth.REQUIRE).build();
+                            pipeline.addLast(new SslHandler(sslContext.newEngine(ch.alloc())));
+                        }
                         pipeline.addLast(new NettyMessageDecoder());
                         pipeline.addLast(new NettyMessageEncoder());
                         pipeline.addLast(new IdleStateHandler(ProxyConfig.READER_IDLE_TIME_SECONDS, ProxyConfig.WRITER_IDLE_TIME_SECONDS, ProxyConfig.ALL_IDLE_TIME_SECONDS));
@@ -85,35 +105,8 @@ public class ServerApplication implements Application {
                         pipeline.addLast(new HeartBeatServerHandler());
                     }
                 });
-        bootstrap.bind(ProxyConfig.getServerPort()).get();
+        bootstrap.bind(port).get();
     }
-
-    public void initSSLBootstrapServer(TLSConfig tls) throws InterruptedException, IOException, ExecutionException {
-        File crt = new File(tls.getKeyCertChainFile());
-        File key = new File(tls.getKeyFile());
-        File ca = new File(tls.getCaFile());
-        SslContext sslContext = SslContextBuilder.forServer(crt, key).trustManager(ca).clientAuth(ClientAuth.REQUIRE).build();
-        ServerBootstrap bootstrap = new ServerBootstrap();
-        bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
-                        ChannelPipeline pipeline = ch.pipeline();
-                        pipeline.addLast(new SslHandler(sslContext.newEngine(ch.alloc())));
-                        pipeline.addLast(new NettyMessageDecoder());
-                        pipeline.addLast(new NettyMessageEncoder());
-                        pipeline.addLast(new IdleStateHandler(ProxyConfig.READER_IDLE_TIME_SECONDS, ProxyConfig.WRITER_IDLE_TIME_SECONDS, ProxyConfig.ALL_IDLE_TIME_SECONDS));
-                        pipeline.addLast(new ServerLoginAuthHandler(userApplications));
-                        pipeline.addLast(new ServerDisconnectHandler());
-                        pipeline.addLast(new ServerTunnelConnectHandler());
-                        pipeline.addLast(new ServerTunnelTransferHandler());
-                        pipeline.addLast(new HeartBeatServerHandler());
-                    }
-                });
-
-        bootstrap.bind(tls.getPort()).get();
-    }
-
 
     @Override
     public void stop() throws Exception {
